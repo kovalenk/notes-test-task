cdd3d6718a64a7a9784cf733dae313636f69c992
kovalenk <yura.kovalenko.1996.29@gmail.com>
2024-01-11 03:12:06 +0200

    Updates

--- assets/modalActions-Hv7rTiPa.js
+++
@@ -1,1 +0,0 @@
-import{l}from"./index-srA58fo7.js";var t=(e=>(e.AddNote="add-note",e.DeleteNote="delete-note",e))(t||{});const o=l({}),a=l(),n=e=>{a.value=e,o.value[a.value]=!o.value[a.value]},r=e=>{a.value=e,o.value[a.value]=!1},u=e=>o.value[e];export{t as E,a,r as c,u as i,o as m,n as o};
---
+++ assets/modalActions-mHzadwKV.js
@@ -0,0 +1,1 @@
+import{l}from"./index-H8Cx579Z.js";var t=(e=>(e.AddNote="add-note",e.DeleteNote="delete-note",e))(t||{});const o=l({}),a=l(),n=e=>{a.value=e,o.value[a.value]=!o.value[a.value]},r=e=>{a.value=e,o.value[a.value]=!1},u=e=>o.value[e];export{t as E,a,r as c,u as i,o as m,n as o};
